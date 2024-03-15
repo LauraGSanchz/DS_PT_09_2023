79137ab7362968e15713394a9d7ac9ddef9a2901
LauraGSanchz <145982662+LauraGSanchz@users.noreply.github.com>
2024-03-15 21:00:46 +0100

    .

--- Data_Engineer/Streamlit/Teoria/app/app2.py
+++ Data_Engineer/Streamlit/Teoria/app/app2.py
@@ -1,16 +1,5 @@
 import streamlit as st
 
-<<<<<<< HEAD
-st.title("Hola Mundo, esta es una app de streamlit")
-
-st.subheader("Cuerpo de la app")
-
-st.text("Esto es un texto")
-
-st.text('esto se puede utilizar para poner lo que queramos')
-
-st.balloons()
-=======
 st.title('esto es un titulo')
 
 st.subheader('esto es un subheader')
@@ -18,4 +7,3 @@ st.subheader('esto es un subheader')
 st.text('esto es un texto')
 
 st.text('esto se puede usar para otras cosas')
->>>>>>> upstream/main
